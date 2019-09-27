8fb74132d28dfa3887524609f27955c15eef599d
Mariana Bálsamo <mariana.balsamo@intive.com>
2019-09-27 10:41:05 -0300

    Hook!

--- Desktop/createApp/proyecto/src/App.js
+++ Desktop/createApp/proyecto/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 //import EjercicioOcho from './components/EjercicioOcho';
-import EjercicioNueve from './components/EjercicioNueve';
+//import EjercicioNueve from './components/EjercicioNueve';
+//import EjercicioOnce from './components/EjercicioOnce';
 // import EjercicioUno from './components/EjercicioUno';
 // import EjercicioDos from './components/EjercicioDos';
 //import EjercicioTres from './components/EjercicioTres';
 //import EjercicioCuatro from './components/EjercicioCuatro';
+import EjercicioDosHooks from './components/EjercicioDosHooks';
 
 class App extends Component {
 
@@ -21,7 +23,9 @@ class App extends Component {
       // <EjercicioTres name={this.state.name}/>
       //<EjercicioCuatro/>
       //<EjercicioOcho/>
-      <EjercicioNueve/>
+      //<EjercicioNueve/>
+      //<EjercicioOnce/>
+      <EjercicioDosHooks/>
     );
   }
 }
